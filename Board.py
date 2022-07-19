763508396de13ebf40b12baad5b9044f7c9d41b2
rohanshah23 <109247825+rohanshah23@users.noreply.github.com>
2022-07-19 17:59:48 -0400

    Update Board.py

--- Board.py
+++ Board.py
@@ -1,10 +1,10 @@
-import random
-
 playerboard = [
     ['-','-','-'],
     ['-','-','-'],
     ['-','-','-']
 ]
+
+
 
 user = 1
 
@@ -24,13 +24,21 @@ def check_piece(row, column):
 def tic_tac_toe():
     print_the_board(playerboard)
     turn()
+    result = game_result(playerboard)
+    if result == "X":
+        print("User 1 won")
+    elif result == "O":
+        print("User 2 won")
+      
+        
+        
     tic_tac_toe()
 
 def turn():
     global user, playerboard
     invalid = True
     while invalid:
-        print("Player " + str(user) + "'s turn")
+        print("User " + str(user) + "'s turn")
         row = int(input("Row: "))
         column = int(input("Column: "))
         invalid = check_piece(row - 1, column - 1)
@@ -42,5 +50,19 @@ def turn():
         user = 1
     else:
         user = 2
+        
+        
+def game_result(playerboard):
+    for i in range(3):
+        if len(set(playerboard[i])) == 1:
+            return playerboard[i][0]
+    for x in range(3):
+        if len(set([i[x] for i in playerboard])) == 1:
+            return playerboard[0][x]
+    if playerboard[0][0] == playerboard[1][1] == playerboard[2][2] or playerboard[0][2] == playerboard[1][1] == playerboard[2][0]:
+        return playerboard[1][1]
+        
 
 tic_tac_toe()
+
+    
