2affcf5f3a3738690e22872cfb728f82b57a04c2
rohanshah23 <109247825+rohanshah23@users.noreply.github.com>
2022-07-19 16:56:52 -0400

    Create Board.py

---
+++ Board.py
@@ -0,0 +1,13 @@
+import random
+
+playerboard = [
+    ['-','-','-'],
+    ['-','-','-'],
+    ['-','-','-']
+]
+
+def print_the_board(playerboard):
+    for i in playerboard:
+        print(i)
+
+print_the_board(playerboard) 
